a88c7d9cbab69874a876ba2938cb330109ed5626
Junaid Qureshi <Junaid.562000@gmail.com>
2022-02-22 18:34:31 +0530

    add toastify to show errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar';
 import Explore from './pages/Explore';
 import ForgotPassword from './pages/ForgotPassword';
@@ -21,6 +23,7 @@ const App = () => {
         </Routes>
         <Navbar />
       </Router>
+      <ToastContainer />
     </>
   );
 };
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { ReactComponent as ArrowRightComponent } from '../assets/svg/keyboardArrowRightIcon.svg';
+import { toast } from 'react-toastify';
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -44,7 +45,7 @@ const SignUp = () => {
       await setDoc(doc(db, 'users', user.uid), formDataCopy);
       navigate('/');
     } catch (e) {
-      console.log(e);
+      toast.error('Somthing went wrong with registration');
     }
   };
   return (
